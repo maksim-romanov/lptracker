49a77b575cda22c6eb10f11043033ce0d9bc1c3a
Maksim Romanov <maksim.romanov@jetrockets.com>
2025-09-05 13:06:25 +0700

    chore: add keyboard-controller

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,41 +2,40 @@ import "react-native-reanimated";
 
 import React from "react";
 
-import { ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { UnistylesRuntime, withUnistyles } from "react-native-unistyles";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 
 // SplashScreen.preventAutoHideAsync();
 
-const UniThemeProvider = withUnistyles(ThemeProvider, (theme) => ({
-  value: {
-    ...theme,
-    dark: UnistylesRuntime.themeName === "dark", // Automatically detect dark theme
-    colors: {
-      primary: theme.colors.interactiveActive, // #FF007A - Uniswap pink
-      background: theme.colors.background, // Main background
-      card: theme.colors.backgroundModule, // Card/surface background
-      text: theme.colors.textPrimary, // Primary text
-      border: theme.colors.border, // Borders and dividers
-      notification: theme.colors.error, // Badges and notifications
-    },
-    fonts: {
-      regular: {},
-      medium: {},
-    },
-  } as ReactNavigation.Theme,
-}));
+// const UniThemeProvider = withUnistyles(ThemeProvider, (theme) => ({
+//   value: {
+//     ...theme,
+//     dark: UnistylesRuntime.themeName === "dark", // Automatically detect dark theme
+//     colors: {
+//       primary: theme.colors.interactiveActive, // #FF007A - Uniswap pink
+//       background: theme.colors.background, // Main background
+//       card: theme.colors.backgroundModule, // Card/surface background
+//       text: theme.colors.textPrimary, // Primary text
+//       border: theme.colors.border, // Borders and dividers
+//       notification: theme.colors.error, // Badges and notifications
+//     },
+//     fonts: {
+//       regular: {},
+//       medium: {},
+//     },
+//   },
+// }));
 
 export default function RootLayout() {
   return (
-    <UniThemeProvider>
+    <KeyboardProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
 
       <StatusBar style="auto" />
-    </UniThemeProvider>
+    </KeyboardProvider>
   );
 }
