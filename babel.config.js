8d7e3fe12dd4d4f5b8f93ae62019c8d818b81bb4
Maksim Romanov <maksim.romanov@jetrockets.com>
2025-09-05 13:02:45 +0700

    chore: add reanimated & worklets

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["react-native-unistyles/plugin", { root: "src" }]],
+    plugins: [["react-native-unistyles/plugin", { root: "src" }], "react-native-worklets/plugin"],
   };
 };
